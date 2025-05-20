f0ee30db380cd46aa546983a9c3b8722b92271a6
Youlong <1041577455@qq.com>
2025-05-20 16:01:45 +0800

    基础的spring环境搭建

---
+++ src/main/java/com/pet/Test/HelloController.java
@@ -0,0 +1,16 @@
+package com.pet.Test;
+
+import com.pet.common.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @PostMapping("name")
+    public String hello(@RequestBody(required = false) User user){
+        return user.getName();
+    }
+}
---
+++ src/main/java/com/pet/common/User.java
@@ -0,0 +1,8 @@
+package com.pet.common;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name="li";
+}
